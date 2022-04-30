1c18b880310b721a44060c71f1575a1cdb84555e
ferschoneich01 <ferschoneich@gmail.com>
2022-04-30 14:17:31 +0600

    especialidades y horarios funcionales

--- application.py
+++ application.py
@@ -1,3 +1,4 @@
+from pydoc import describe
 from flask import Flask, render_template, request, redirect, url_for, flash
 import cx_Oracle
 
@@ -45,9 +46,23 @@ def Medicos():
         return render_template('medicos.html',medicos=rows)
 
 #ruta especialidades
-@app.route('/especialidades')
-def Especialidades():        
-    return render_template('especialidades.html')
+@app.route('/especialidades', methods=["GET"])
+def Especialidades():   
+    if request.method == "GET":
+        try:
+            cursor=conexion.cursor()
+            #cursor.execute("select id_medico_especialidad,m.nombre,e.nombre from xdb.medico_especialidad me inner join xdb.medicos m on me.id_medico = m.id_medico INNER JOIN xdb.especialidades e on e.id_especialidad = me.id_especialidad")
+            #Mespecialidades=cursor.fetchall()
+            cursor.execute("select * from xdb.medicos") 
+            medicos=cursor.fetchall()
+            cursor.execute("select * from xdb.especialidades") 
+            especialidades=cursor.fetchall()
+        except Exception as err:
+            print(err)
+            return redirect('/especialidades')
+        else:
+            cursor.close()     
+    return render_template('especialidades.html',medicos=medicos, especialidades=especialidades)
 
 #ruta medicos
 @app.route('/horarios', methods=["GET"])
@@ -55,7 +70,7 @@ def Horarios():
     if request.method == "GET":
         try:
             cursor=conexion.cursor()
-            cursor.execute("select * from xdb.horario")
+            cursor.execute("select id_horario,m.nombre,fechaatencio,inicioatencion,finatencion from xdb.horario h inner join xdb.medicos m on m.id_medico = h.medico")
             horarios=cursor.fetchall()
             cursor.execute("select * from xdb.medicos") 
             medicos=cursor.fetchall()
@@ -83,7 +98,7 @@ def addMedico():
         
         try:      
             cursor=conexion.cursor()
-            query = "insert into xdb.medicos values((SELECT count(*)+1 FROM xdb.medicos),'"+nombre+"','"+apellido+"','"+dni+"','"+direccion+"','"+correo+"','"+telefono+"','"+sexo+"','"+numcol+"',TO_DATE('"+fechanac+"','YYYY/MM/DD'),1)"
+            query = "insert into xdb.medicos values(NULL,'"+nombre+"','"+apellido+"','"+dni+"','"+direccion+"','"+correo+"','"+telefono+"','"+sexo+"','"+numcol+"',TO_DATE('"+fechanac+"','YYYY/MM/DD'),1)"
             cursor.execute(query)
             
         except Exception as err:
@@ -105,8 +120,7 @@ def addHorario():
         print(horaFin)
         try:      
             cursor=conexion.cursor()
-
-            query = "insert into xdb.horario values((SELECT count(*)+1 FROM xdb.horario),"+medico+",TO_DATE('"+fechaAt+"','YYYY/MM/DD'),'TO_DATE('"+fechaAt+"','YYYY/MM/DD') "+horaInicio+":00',TO_DATE('"+fechaAt+"','YYYY/MM/DD') "+horaFin+":00)"
+            query = "insert into xdb.horario values(NULL,"+medico+",TO_DATE('"+fechaAt+"','YYYY/MM/DD'),TO_TIMESTAMP('"+horaInicio+":00', 'HH24:MI:SS'),TO_TIMESTAMP('"+horaFin+":00', 'HH24:MI:SS'))"
             cursor.execute(query)
             
         except Exception as err:
@@ -116,7 +130,63 @@ def addHorario():
             print('datos insertados')
             conexion.commit()
             cursor.close()
-            return redirect('/horario')
+            return redirect('/horarios')
+
+@app.route('/addMEspecialidad',methods=["POST","GET"])
+def addMEspecialidad():
+    if request.method == "POST":
+        medico = request.form["medico"]
+        especialidad = request.form["especialidad"]
+        
+        try:      
+            cursor=conexion.cursor()
+            query = "insert into xdb.medico_especialidad values(NULL,"+medico+","+especialidad+")"
+            cursor.execute(query)
+            
+        except Exception as err:
+            print(err)
+            return redirect('/especialidades')
+        else:
+            print('datos insertados')
+            conexion.commit()
+            cursor.close()
+            return redirect('/especialidades')
+
+@app.route('/addEspecialidad',methods=["POST","GET"])
+def addEspecialidad():
+    if request.method == "POST":
+        nombre = request.form["Nombre"]
+        descripcion= request.form["Descripcion"]
+        
+        try:      
+            cursor=conexion.cursor()
+            query = "insert into xdb.especialidades values(NULL,'"+nombre+"','"+descripcion+"')"
+            cursor.execute(query)
+            
+        except Exception as err:
+            print(err)
+            return redirect('/especialidades')
+        else:
+            print('datos insertados')
+            conexion.commit()
+            cursor.close()
+            return redirect('/especialidades')
+            
+@app.route('/deleteEspecialidad/<string:id>', methods=["GET","POST"])
+def deleteEspecialidad(id):        
+        try:  
+            cursor=conexion.cursor()   
+            cursor.execute('delete from especialidades where id_especialidad = {0}'.format(id))
+            print('eliminando...')
+        except Exception as err:
+            print(err)
+            return redirect('/especialidades')
+        else:
+
+            print('especialidad eliminada')
+            conexion.commit()
+            cursor.close()
+            return redirect('/especialidades')
 
 @app.route('/deleteMedico/<string:id>', methods=["GET","POST"])
 def deleteMedico(id):        
@@ -133,6 +203,22 @@ def deleteMedico(id):
             conexion.commit()
             cursor.close()
             return redirect('/medicos')
+
+@app.route('/deleteHorario/<string:id>', methods=["GET","POST"])
+def delete(id):        
+        try:  
+            cursor=conexion.cursor()   
+            cursor.execute('delete from horario where id_horario = {0}'.format(id))
+            print('eliminando...')
+        except Exception as err:
+            print(err)
+            return redirect('/horarios')
+        else:
+
+            print('horario eliminado')
+            conexion.commit()
+            cursor.close()
+            return redirect('/horarios')
     
         
 if __name__ == '__main__':
